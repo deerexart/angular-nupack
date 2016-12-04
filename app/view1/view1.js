c1154b0da83cf504c9b12e1944cd753d6b152ae9
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-04 16:16:17 -0500

    view1.html -> added ng-model to start rate input markups.startRate -> replaced placeholder text for Job Mark up & People with their respective scope objects & used currencty filter
    view1.js -> created service for job mark up & number of people, using number inputted
    -> MarkupCtrl -> created to handle calculated values from service
    -> made scope object : $scope.markups to house Start Rate/Job Markup / & people, & for future markups
    
    	modified:   app/view1/view1.html
    	modified:   app/view1/view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,9 +9,49 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('MarkupCtrl', [function() {
+.controller('MarkupCtrl', ['$scope', 'markupAmounts', function($scope, markupAmounts) {
 // returned scope object w/ markup values
+
+$scope.markups = {
+  startRate: 1299.99, // inital value
+  numberOfPeople: 3, //
+
+  price: function(){
+    var flatRateCalculation = markupAmounts.flatRate(this.startRate)
+    return flatRateCalculation
+  },
+  people: function(){
+    var numberOfPeople = this.numberOfPeople;
+    var peopleCalculation = markupAmounts.noOfPeople(this.numberOfPeople, this.startRate);
+    return peopleCalculation;
+  }
+}
+
 }])
 .service('markupAmounts', function(){
   //values calculation functions
+// flat rate = job markup 5% + initial cost
+  var flatRate = function(startRate){
+    var getStartRate = {
+      startRate: startRate
+    };
+    var jobCostCalculation = startRate * .05;
+    return jobCostCalculation
+  }
+
+  var noOfPeople = function(numberOfPeople, startRate){
+    var getMarkups = {
+      startRate:startRate,
+      numberOfPeople:numberOfPeople
+    }
+    var peopleCalculation = (parseFloat(flatRate(startRate)) + parseFloat(startRate)) *  (numberOfPeople * (1.2/100));
+
+    return peopleCalculation
+  }
+
+return {
+  flatRate: flatRate,
+  noOfPeople: noOfPeople
+}
+
 })
