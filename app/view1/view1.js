fda29548093b6e62d761d4e978a4cf3e381c3786
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-04 16:03:12 -0500

    made controller and service spots

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,13 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'MarkupCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('MarkupCtrl', [function() {
+// returned scope object w/ markup values
+}])
+.service('markupAmounts', function(){
+  //values calculation functions
+})
