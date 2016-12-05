9e941735f61376de6bfc7da2d51f06caa4c7cbb5
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-05 18:26:37 -0500

    mocked up scope object to run tests on Test block checks that object type matches expected object type Checks that price isn't a string checks that a startRate has been set (1299.99) 	modified:   app/view1/view1_test.js

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -30,6 +30,19 @@ describe('MarkupCtrl', function(){
 
  }));
 
+ beforeEach(function() {
+   //mocking up scope object
+     markups = {
+       startRate: 1299.99,
+       numberOfPeople: 3,
+       price: Function,
+       people: Function,
+       isFood:Function,
+       isElectronic:Function,
+       isPharm: Function,
+       finalCost:Function
+     };
+   });
 
  describe("MarkupCtrl controller to be definedddd", function() {
 
@@ -40,7 +53,37 @@ describe('MarkupCtrl', function(){
      it("$scope.markups should be defined ", function () {
          expect($scope.markups).toBeDefined();
      });
+     //tests on scope object
+     it("matches object type with the expect key/value pairs", function() {
+         expect(markups).toEqual(jasmine.objectContaining({
+           startRate: 1299.99
+         }));
+         expect(markups).not.toEqual(jasmine.objectContaining({
+           price: ''
+         }));
+         expect(markups).toEqual(jasmine.objectContaining({
+          //  startRate:Number, //will fail
+           startRate:1299.99, // will pass
+           numberOfPeople: 3,
+           people: Function,
+           price: Function,
+           isFood: Function,
+           isElectronic: Function,
+           isPharm: Function,
+           finalCost: Function
+         }))
+          expect(markups.startRate).toEqual(jasmine.any(Number));
+          expect(markups.numberOfPeople).toEqual(jasmine.any(Number));
+          expect(markups.people).toEqual(jasmine.any(Function));
+          expect(markups.price).toEqual(jasmine.any(Function));
+          expect(markups.isFood).toEqual(jasmine.any(Function));
+          expect(markups.isElectronic).toEqual(jasmine.any(Function));
+          expect(markups.isPharm).toEqual(jasmine.any(Function));
+          expect(markups.finalCost).toEqual(jasmine.any(Function));
 
+
+
+       });
 
     });
 
@@ -67,7 +110,7 @@ describe('MarkupCtrl', function(){
         expect(service).toEqual(jasmine.any(Object));
         });
 
-      
+
 
       });
     });
