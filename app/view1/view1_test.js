d89023918ade72aa20e8363e97c9cf73ae8ebf0d
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-05 18:23:48 -0500

    Ran tests to check if controller and service in controller are defined Ran test to check if service is defined Check if service is an object
    	modified:   app/view1/view1_test.js

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,6 +6,7 @@ describe('MarkupCtrl', function(){
     var $rootScope, $scope, $controller;
     var markupAmounts;
     var MarkupCtrl;
+    var markups;
 
   beforeEach(module('myApp.view1'));
 
@@ -32,12 +33,43 @@ describe('MarkupCtrl', function(){
 
  describe("MarkupCtrl controller to be definedddd", function() {
 
-        it("should be  defined", function () {
-            expect(MarkupCtrl).toBeDefined();
-        });
+
+     it("MarkupCtrl controller should be defined", function () {
+         expect(MarkupCtrl).toBeDefined();
+     });
+     it("$scope.markups should be defined ", function () {
+         expect($scope.markups).toBeDefined();
+     });
+
 
     });
 
+    describe(' testing for service ...', function() {
+      describe('markupAmounts service mock', function() {
+        var createService;
+      // beforeEach(module('myApp.view1'));
+        beforeEach(inject(function() {
+          var $injector = angular.injector(['myApp.view1'])
 
+          createService = function() {
+            return $injector.get('markupAmounts');
+          }
+        }));
+
+        it('service should be defined', function(){
+          var service = createService();
+          expect(service).toBeDefined();
+        });
+
+
+        it('service should be an object', function(){
+          var service = createService();
+        expect(service).toEqual(jasmine.any(Object));
+        });
+
+      
+
+      });
+    });
 
 });
